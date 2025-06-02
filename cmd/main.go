b540b5cee228d729923ac11d89b0d63fb3c7c497
IvanDrf <ivandrgmr@gmail.com>
2025-06-03 02:14:29 +0300

    feat: add post handler

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"log"
 
+	"github.com/IvanDrf/units/internal/handlers"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	server := echo.New()
+
+	server.POST("/", handlers.PostHandler)
 
 	if err := server.Start(":8080"); err != nil {
 		log.Fatal(err)
---
+++ internal/handlers/handlers.go
@@ -0,0 +1,33 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/IvanDrf/units/internal/convert"
+	"github.com/IvanDrf/units/internal/models"
+	"github.com/labstack/echo/v4"
+)
+
+func PostHandler(ctx echo.Context) error {
+	req := models.Request{}
+
+	if err := ctx.Bind(&req); err != nil {
+		return ctx.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid body request"})
+	}
+
+	result := models.Responce{
+		UnitsType: req.UnitsType,
+		Units:     req.Units,
+		Value:     req.Value,
+		NewUnits:  req.NewUnits,
+		NewValue:  -1,
+	}
+
+	var err error
+	result.NewValue, err = convert.Convert(&req)
+	if err != nil {
+		return ctx.JSON(http.StatusBadRequest, map[string]string{"error": err.Error()})
+	}
+
+	return ctx.JSON(http.StatusOK, result)
+}
