d231a0cb3e511dad2dc4b0cdca600084e693d16a
IvanDrf <ivandrgmr@gmail.com>
2025-06-10 17:31:29 +0300

    feat: add methods for logger

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,10 +1,28 @@
 package logger
 
 import (
+	"fmt"
 	"log/slog"
 	"os"
 )
 
-func InitLogger() *slog.Logger {
-	return slog.New(slog.NewJSONHandler(os.Stdout, nil))
+type Logger struct {
+	l *slog.Logger
+}
+
+func (log *Logger) InfoLevel(method, msg string) {
+	log.l.Info(fmt.Sprintf("%s: %s", method, msg))
+}
+
+func (log *Logger) ErrorLevel(method, msg string) {
+	log.l.Info(fmt.Sprintf("%s: %s", method, msg))
+}
+
+func (log *Logger) Fatal(msg string) {
+	log.l.Error(msg)
+	os.Exit(1)
+}
+
+func InitLogger() Logger {
+	return Logger{slog.New(slog.NewJSONHandler(os.Stdout, nil))}
 }
