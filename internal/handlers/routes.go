6c9a28ec8aec987c5fae502202000fc4046d9f94
IvanDrf <ivandrgmr@gmail.com>
2025-06-26 21:09:47 +0300

    feat: add /api in routes

--- internal/handlers/routes.go
+++ internal/handlers/routes.go
@@ -1,8 +1,8 @@
 package handlers
 
 func (s *Server) RegisterRoutes() {
-	s.Server.POST("/", s.PostHandler)
-	s.Server.GET("/", s.GetHandler)
-	s.Server.PATCH("/conversions/:id", s.PatchHandler)
-	s.Server.DELETE("/conversions/:id", s.DeleteHandler)
+	s.Server.POST("/api", s.PostHandler)
+	s.Server.GET("/api", s.GetHandler)
+	s.Server.PATCH("/api/conversions/:id", s.PatchHandler)
+	s.Server.DELETE("/api/conversions/:id", s.DeleteHandler)
 }
