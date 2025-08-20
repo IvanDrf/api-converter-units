8fc7871a6f3450c4aff22441ebb9b832866104be
Ivan Drf <ivandrgmr@gmail.com>
2025-08-20 23:31:42 +0300

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"github.com/IvanDrf/units/internal/database"
 	"github.com/IvanDrf/units/internal/handlers"
 )
 
 func main() {
-	database.InitDB()
 	server := handlers.InitServer()
 
 	server.RegisterRoutes()
