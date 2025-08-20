26cc2cfab751b84ab19bf4ff4229eb4046023ffa
Ivan Drf <ivandrgmr@gmail.com>
2025-08-20 23:30:34 +0300

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,28 +1,13 @@
 package main
 
 import (
-	"sync"
-
 	"github.com/IvanDrf/units/internal/database"
 	"github.com/IvanDrf/units/internal/handlers"
 )
 
 func main() {
-	server := new(handlers.Server)
-	wg := new(sync.WaitGroup)
-
-	wg.Add(2)
-	go func() {
-		defer wg.Done()
-		server = handlers.InitServer()
-	}()
-
-	go func() {
-		defer wg.Done()
-		database.InitDB()
-	}()
-
-	wg.Wait()
+	database.InitDB()
+	server := handlers.InitServer()
 
 	server.RegisterRoutes()
 	server.Start()
